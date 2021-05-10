8cb76024990ca9ad14127bd90bb82701df8d4c3c
manuel <manuel.gutierrez@lifebeacon.com>
2021-05-10 14:48:55 -0500

    Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="uk-container">
+        <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+            <div>
+                <h1>Title</h1>    
+            </div>
+            <div>
+                Image
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
